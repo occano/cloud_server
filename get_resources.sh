cfaa658fc1c7237868784a426ac5b75537f62bb1
ophir-occano <51914818+ophir-occano@users.noreply.github.com>
2019-09-15 17:00:34 +0300

    Update get_resources.sh

--- get_resources.sh
+++ get_resources.sh
@@ -2,3 +2,5 @@
 SERVICE_NAME=trainer
 mkdir -p resources
 gsutil cp -r -n gs://occano-resources/ds-resources/$SERVICE_NAME/* resources/
+gsutil cp -r -n gs://occano-resources/ds-resources/head_models/* resources/
+
