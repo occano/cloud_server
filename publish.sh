5520d7e4d1262a441f31b3d19e09bc2d10e0db2a
ophir-occano <ophir@occano.io>
2020-04-29 12:51:52 +0300

    fix publish from Nuc

--- publish.sh
+++ publish.sh
@@ -3,4 +3,6 @@ DOCKER_IMAGE=eu.gcr.io/vessel-engine-monitor/cloud-server
 gcloud auth configure-docker
 
 docker build -t $DOCKER_IMAGE .
-docker push $DOCKER_IMAGE
+#docker push $DOCKER_IMAGE
+cd ~/cloud-server
+docker-compose up
