2121d4c1ac6abac27d40a5357453654127f6f1d8
ophir-occano <ophir@occano.io>
2020-05-07 15:56:32 +0300

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DOCKER_IMAGE=eu.gcr.io/vessel-engine-monitor/cloud-server
-gcloud auth configure-docker
+DOCKER_IMAGE=occano/cloud-server
+#gcloud auth configure-docker
 
 docker build -t $DOCKER_IMAGE .
-#docker push $DOCKER_IMAGE
-cd ~/cloud-server
-docker-compose restart
+docker push $DOCKER_IMAGE
+docker-compose down
+docker-compose up
